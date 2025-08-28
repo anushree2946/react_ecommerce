a7de69642f6723b4fda9f33375b4688a78756145
anushree241 <241ca007@srcw.ac.in>
2025-08-28 21:05:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import SignupScreen from "./components/screens/SignupScreen";
+import LoginScreen from "./components/screens/LoginScreen";
+import ProductDetails from "./components/screens/ProductDetails";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+            <Routes>
+              <Route path="/product/:id" element={<ProductDetails />} />
+            </Routes>
+            <Routes>
+              <Route path="/signup" element={<SignupScreen />} />
+            </Routes>
+            <Routes>
+              <Route path="/login" element={<LoginScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
---
+++ src/products.js
@@ -0,0 +1,84 @@
+const products = [
+    {
+      '_id': '1',
+      'name': 'Airpods Wireless Bluetooth Headphones',
+      'image': '/images/Airpods.jpg',
+      'description':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      'brand': 'Apple',
+      'category': 'Electronics',
+      'price': 89.99,
+      'countInStock': 0,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'Apple iPhone 16 ',
+      'image': '/images/phone.jpg',
+      'description':
+        '5G Mobile Phone with Camera Control, A18 Chip and a Big Boost in Battery Life. Works with AirPods; Pink',
+      'brand': 'Apple',
+      'category': 'Electronics',
+      'price': 82600,
+      'countInStock': 7,
+      'rating': 4.0,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'Fujifilm X-T10 Black Camera',
+      'image': '/images/camera.jpg',
+      'description':
+        'Mirrorless Digital Camera Kit with XF 18-55mm F2.8-4.0 R LM OIS Lens',
+      'brand': 'Fujifilm',
+      'category': 'Electronics',
+      'price': 150000,
+      'countInStock': 5,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'Sony Playstation 4 Pro White Version',
+      'image': '/images/playstation.jpg',
+      'description':
+        'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      'brand': 'Sony',
+      'category': 'Electronics',
+      'price': 399.99,
+      'countInStock': 11,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '5',
+      'name': 'Logitech G-Series Gaming Mouse',
+      'image': '/images/mouse.jpg',
+      'description':
+        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      'brand': 'Logitech',
+      'category': 'Electronics',
+      'price': 49.99,
+      'countInStock': 7,
+      'rating': 3.5,
+      'numReviews': 10,
+    },
+    {
+      '_id': '6',
+      'name': 'Amazon Echo Dot (5th Gen)',
+      'image': '/images/alexa.jpeg',
+      'description':
+        'Smart speaker with Bigger sound, Motion Detection, Temperature Sensor, Alexa and Bluetooth| Blue',
+      'brand': 'Amazon',
+      'category': 'Electronics',
+      'price': 5500,
+      'countInStock': 2,
+      'rating': 4,
+      'numReviews': 12,
+    },
+  ]
+  
+  
+  export default products
+  
